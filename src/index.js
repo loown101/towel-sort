d30dc7fe62403124e4f77669f21e45c18a7f4ea8
loown101 <loown101@gmail.com>
2021-03-01 21:33:52 +0300

    fix: add "matrix==null"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
   let sortArr = [];
+    if (matrix == null) {
+      return sortArr;
+    } 
+  
    for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
 
